8c42f5ffb5c917bb65ca945e612db51389271e53
Kumar manak <manakkash20@gmail.com>
2025-12-31 16:11:07 +0530

    chore: fix prisma v7 config and database connection

---
+++ prisma/migrations/20251231103952_init_job_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Job" (
+    "id" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "payload" JSONB NOT NULL,
+    "status" TEXT NOT NULL DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
+);
